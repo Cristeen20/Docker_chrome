55be1982788261bba75d34917d0fc7d804033ffd
Cristeen20 <cristeena121@gmail.com>
2024-10-23 19:22:42 -0400

    docker selenium

--- app.py
+++ app.py
@@ -1,39 +1,47 @@
 from flask import Flask, jsonify, request
-import asyncio
-from pyppeteer import launch
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 
 app = Flask(__name__)
 
-# Function to capture screenshot using Pyppeteer
-async def capture_screenshot(url):
-    browser = await launch(handleSIGINT=False,
-                            handleSIGTERM=False,
-                            handleSIGHUP=False)
-    page = await browser.newPage()
-    await page.goto(url)
+# Function to capture screenshot using Selenium
+def capture_screenshot(url):
+    # Set up Chrome options
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")  # Run in headless mode
+    chrome_options.add_argument("--no-sandbox")  # Overcome limited resource problems
+    chrome_options.add_argument("--disable-dev-shm-usage")  # Overcome limited resource problems
+
+    # Set up the WebDriver (using Chrome)
+    CHROME_PATH = ""
+    driver = webdriver.Chrome(service=Service(CHROME_PATH), options=chrome_options)
+
+    # Navigate to the URL and take a screenshot
+    driver.get(url)
     screenshot_path = 'screenshot.png'
-    await page.screenshot({'path': screenshot_path})
-    await browser.close()
+    driver.save_screenshot(screenshot_path)
+    driver.quit()
+    
     return screenshot_path
 
-@app.route('/', methods=['POST','GET'])
+@app.route('/', methods=['POST', 'GET'])
 def load():
     return "docker trials"
 
 # API endpoint to take a screenshot
-@app.route('/screenshot', methods=['POST','GET'])
+@app.route('/screenshot', methods=['POST', 'GET'])
 def screenshot():
     try:
         url = 'https://www.tripadvisor.ca/AITripBuilder'
         if not url:
             return jsonify({"error": "URL is required"}), 400
 
-        # Launch Pyppeteer and capture screenshot
-        
+        # Capture screenshot
         try:
-            asyncio.set_event_loop(asyncio.SelectorEventLoop())
-            screenshot_path = asyncio.get_event_loop().run_until_complete(capture_screenshot(url))
-        except Exception as e:  # If there is no event loop in the current thread
+            screenshot_path = capture_screenshot(url)
+        except Exception as e:
             return jsonify({"error": str(e)}), 500
         
         return jsonify({"screenshot_path": screenshot_path}), 200
